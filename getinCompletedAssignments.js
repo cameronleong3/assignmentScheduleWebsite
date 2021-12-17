bd89386592d0fde7a1964e6b5412c57fcde61465
cameronleong3 <44122273+cameronleong3@users.noreply.github.com>
2021-12-17 12:33:18 -0800

    Delete getinCompletedAssignments.js

--- getinCompletedAssignments.js
+++
@@ -1,9 +0,0 @@
-const sendResponse = require("./utils/send-response");
-
-const getinCompletedAssignments = (req, res) => {
-  return req.app.db.getinCompletedAssignments().then((tasks) => {
-    sendResponse(res, 200, { tasks });
-  });
-};
-
-module.exports = getinCompletedAssignments;
